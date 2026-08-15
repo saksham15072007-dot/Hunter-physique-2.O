1cb3bf89e4b2d4a6c3ef138c626289aa99c99e28
saksham15072007-dot <saksham15072007@gmail.com>
2026-08-15 12:29:33 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="hunter-system-fixed-v2";
+const CACHE="hunter-system-fixed-v3";
 const APP_SHELL=["./","./index.html","./manifest.json","./sw.js","./icon-192.png","./icon-512.png","./icon-512-maskable.png"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(APP_SHELL)));self.skipWaiting()});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
